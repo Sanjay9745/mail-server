014ac6df941762b320600cb36dced54206bb7406
Sanjay9745 <sanjaysanthosh919@gmail.com>
2024-03-08 21:07:55 +0530

    new

--- app.js
+++ app.js
@@ -1,33 +1,29 @@
-const SMTPServer = require("smtp-server").SMTPServer;
-const simpleParser = require("mailparser").simpleParser;
-
+const SMTPServer = require('smtp-server').SMTPServer;   
 const server = new SMTPServer({
-  onConnect: (session, callback) => {
-    console.log("Client connected:", session.remoteAddress);
-    callback(); // Accept the connection
-  },
-  onMailFrom: (address, session, callback) => {
-    console.log("Message from:", address.address);
-    callback(); // Accept the address
-  },
-  onRcptTo: (address, session, callback) => {
-    console.log("Message to:", address.address);
-    callback(); // Accept the address
-  },
-  onData: (stream, session, callback) => {
-    simpleParser(stream, {}, (err, parsed) => {
-      if (err) {
-        console.error("Error parsing mail:", err);
-      } else {
-        console.log("Parsed mail:", parsed);
-      }
-    });
-    stream.on("end", callback);
-  },
+        onConnect(session, callback) {
+            console.log('Connected to the server', session.id);
+             callback();
+        },
+        onMailFrom(address, session, callback) {
+            console.log('Mail from:', address.address, session.id);
+            callback();
+        
+        },
+        onRcptTo(address, session, callback) {
+            console.log('Mail to:', address.address, session.id);
+            callback();
+        },
+        onData(stream, session, callback) {
+            console.log('Data', session.id);
+            stream.pipe(process.stdout);
+            stream.on('end', callback);
+        },
+        onClose(session) {
+            console.log('Session ended', session.id);
+        }
 
-  disabledCommands: ["AUTH"],
 });
 
-server.listen(465, "localhost", () => {
-  console.log("SMTP Server is running at localhost:465");
+server.listen(25,()=>{
+    console.log('Server is running on port 25');
 });
